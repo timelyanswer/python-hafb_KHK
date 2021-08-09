6a31488214e41cec933b6464ef12a2df3f64b1d5
t11 <kimjeong@hotmail.com>
2021-08-09 09:49:40 -0600

    third commit

--- Hello.py
+++ Hello.py
@@ -1,10 +1,51 @@
+# python-hafb_KHK
+
+## Instructor project
+http://github.com/jik-wsu/python-hafb_KHK
+
+## Local installations
+
+Login to system using:
+- Username: **txx** (xx is terminal number)
+- Password: **Hello123**
+
+Need to open git bash and run the following comma
+'''bash
+pip install pytest
+'''
+Reload 'Pycharm'
+
 #!/usr/bin/env python3
-# Purpose: Say Hello to Waldo
+"""
+Author: Jeong Kim
+Purpose: Say Hello to Waldo
+"""
+
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help = 'Name to greet')
-args = parser.parse_args()
-print('Hello,  ' + args.name + '!')
+def get_args():
+    """
 
-# print("Hello Waldo")
+    Get the command-line arguments
+    :return: list of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+# add pass for an extra line python expects 2 lines of code in
+
+def main ():
+    """
+    This is where the actions is, python expects 2 lines of code
+    at all times, use 'pass'
+    :return: None
+    """
+    args = get_args()
+    print('Hello,  ' + args.name + '!')
+
+
+if __name__== '__main__':
+    main()
+
