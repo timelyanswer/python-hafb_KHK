ad5d41ec34609917665d40002b0d8d55e1a48eca
t11 <kimjeong@hotmail.com>
2021-08-10 09:53:26 -0600

    Update 2nd day morning

--- Day 2/my_iterables_part2.py
+++ Day 2/my_iterables_part2.py
@@ -6,7 +6,6 @@ Purpose: continue comprehension and iterables
 """
 
 
-
 # --------------------------------------------------
 def main():
     """Make your noise here"""
@@ -16,7 +15,7 @@ def main():
     s = {i for i in range(10)}
     print(s)
     # dict comprehensions: {key_expr: value_expr for item in iterable}
-    d = {i: i*2 for i in range(10)}
+    d = {i: i * 2 for i in range(10)}
     print(d)
 
     # comprehensions: (item or item in iterable)
@@ -30,15 +29,18 @@ def main():
     #        points.append((x,y))
     # print(points)
 
-
     # list comprehension
     points = [(x, y) for x in range(5) for y in range(5)]
     print(points)
 
-
-
-
-
+    # version 1: loops
+    values = []
+    for x in range(100):
+        if x > 50:
+            for y in range(100):
+                if x - y != 0:
+                    values.append(x / (x - y))
+    print(values)
 
 
 # --------------------------------------------------
