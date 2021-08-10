bfe845e97003b59316f8d1fa385401d1e46719a7
t11 <kimjeong@hotmail.com>
2021-08-10 09:56:23 -0600

    Update 2nd day morning

--- Day 2/my_iterables_part2.py
+++ Day 2/my_iterables_part2.py
@@ -42,6 +42,11 @@ def main():
                     values.append(x / (x - y))
     print(values)
 
+    values = [x / (x - y)
+              for x in range(100) if x > 50
+              for y in range(100) if x - y != 0]
+    print(values)
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
