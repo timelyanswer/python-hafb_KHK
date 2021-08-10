916d254f9c6e278d251f82ce43f6cade782c5609
t11 <kimjeong@hotmail.com>
2021-08-10 09:48:11 -0600

    Update 2nd day morning

---
+++ Day 2/my_iterables_part2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Author : t11 <me@wsu.com>
+Date   : 8/10/2021
+Purpose: continue comprehension and iterables
+"""
+
+
+
+# --------------------------------------------------
+def main():
+    """Make your noise here"""
+    # list comprehensions: [expr(item) for item in iterable]
+
+    # set comprehensions: {expr(item) for item in iterable}
+    s = {i for i in range(10)}
+    print(s)
+    # dict comprehensions: {key_expr: value_expr for item in iterable}
+    d = {i: i*2 for i in range(10)}
+    print(d)
+
+    # comprehensions: (item or item in iterable)
+    g = (i for i in range(10))
+    print(g)
+
+    # Multiple if-clauses
+    # points = []
+    # for x in range (5):
+    #    for y in range (x):
+    #        points.append((x,y))
+    # print(points)
+
+
+    # list comprehension
+    points = [(x, y) for x in range(5) for y in range(5)]
+    print(points)
+
+
+
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
--- Day 2/my_itertools.py
+++ Day 2/my_itertools.py
@@ -2,69 +2,64 @@
 """
 Author : t11 <me@wsu.com>
 Date   : 8/10/2021
-Purpose:
+Purpose: Review itertools package
 """
+from itertools import islice, count
+from math import sqrt
 
-import argparse
+def is_prime(x):
+
+    if x < 2:
+        return False
+        for i in range(2, int(sqrt(x)) +1):
+            if x % i == 0:
+                return False
+            return True
 
 
-# --------------------------------------------------
-def get_args():
-    """Get command-line arguments"""
-
-    parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
-    return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
     """Make your noise here"""
+    # Task: Create a list of the first 1000 prime numbers
 
-    args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    # thousand_primes = islice((x for x in count() if is_prime(x)), 1000)
+    # print(thousand_primes)
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+
+    #Use the built-ins any, all
+    print(any([False, False, True]))
+    print(all([False, False, True]))
+    cities = ['London', 'Madrid', 'New York', 'Ogden']
+    print(f'the cities list is{all(city ==city.title() for city in cities)} to go')
+
+    sunday = [12, 14, 15, 15, 17, 21, 22, 22, 23, 22, 20, 18]
+    monday = [13, 14, 14, 16, 20, 21, 22, 22, 21, 19, 18, 16]
+    tuesday = [12, 13, 14, 16, 20, 22, 24, 28, 22, 20, 18, 22]
+
+    # for item in zip(sunday, monday):
+    for sun, mon in zip(sunday, monday):
+        print(f'the average for sunday and monday ={(sun+mon)/2.0}')
+
+
+    for temp in zip(sunday, monday, tuesday):
+        print(f'minimum ={min(temp)}, maximum ={max(temp)} and avg = {sum(temp)/len(temp)}')
+        print(f'minimum ={min(temp):4.1f}, maximum ={max(temp):4.1f} and avg = {sum(temp)/len(temp):4.1f}')
+
+        #print(f'minimum ={:4.1f}, maximum ={:4.1f} and avg = {:4.1f}'.format(min(temp), max(temp), sum))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # --------------------------------------------------
